58b472e102e06a85745ad94e873fb0e62b65184e
Wendy Michelle Sy <wmsy98844@liceo.edu.ph>
2025-07-31 10:08:33 +0800

    gwapa ko

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,57 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Student ID: ");
+        String studentId = scanner.nextLine();
+
+        System.out.print("Enter First Name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Enter Last Name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Enter Course: ");
+        String course = scanner.nextLine();
+
+        System.out.print("Enter Section: ");
+        String section = scanner.nextLine();
+
+        System.out.println("STUDENT INFORMATION");
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Student Name: " + firstName + " " + lastName);
+        System.out.println("Course: " + course);
+        System.out.println("Section: " + section);
+
+        System.out.print("Enter Prelim Exam Score: ");
+        int prelim = scanner.nextInt();
+
+        System.out.print("Enter Midterm Exam Score: ");
+        int midterm = scanner.nextInt();
+
+        System.out.print("Enter Final Exam Score: ");
+        int finalExam = scanner.nextInt();
+
+        System.out.print("Enter Project Score: ");
+        int project = scanner.nextInt();
+
+        System.out.print("Enter Attendance Percentage: ");
+        int attendance = scanner.nextInt();
+
+        int totalScore = midterm + finalExam + project + attendance;
+        double averageScore = (double) totalScore / 400 * 100;
+
+        String remarks = (averageScore < 75) ? "FAILED" : "PASSED";
+
+        System.out.println("STUDENT SCORE");
+        System.out.println("Prelim Exam Score:" + prelim);
+        System.out.println("Midterm Exam Score: " + midterm);
+        System.out.println("Final Exam Score: " + finalExam);
+        System.out.println("Project Score: " + project);
+        System.out.println("Attendance Score: " + attendance);
+        System.out.println("Average Score: " + String.format("%.2f", averageScore));
+        System.out.println("Remarks: " + remarks);
     }
 }
