9120dd05a5c51db64fe7bb774634f4c47bf59399
iAmMitch2003 <wmsy98844@liceo.edu.ph>
2025-08-04 10:19:56 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-import java.util.Scanner;
-
-public class Main {
-    static int number = 0;
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter number " + i+ ": " );
-            number = number+scanner.nextInt();
-
+class Book {
+    String title;
+    String author;
+    int pages;
+    boolean isAvailable;
+    
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isAvailable = true;
+        System.out.println("A new book: " + title + ", by " + author + " has been added to the library.");
+    }
+    
+    public void displayInfo() {  
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Available: " + isAvailable);  
+    }
+    
+    public void borrowBook() {  
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println("You borrowed " + title + ".");
+        } else {
+            System.out.println("Sorry, " + title + " is not available.");
         }
-        System.out.println("Total Number: "+number);
+    }
+    
+    public void returnBook() {  
+        isAvailable = true;
+        System.out.println("You returned " + title + ".");
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book("1984", "George Orwell", 328);  
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", 281);
+        Book book3 = new Book("The Hobbit", "J.R.R. Tolkien", 310);
+
+     	book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
+        
+        book1.borrowBook();
+        book2.borrowBook();
+        book3.borrowBook();
+        
+        book1.returnBook();
+        book2.returnBook();
+        book3.returnBook();
+        
     }
 }
